01fd4d9575feb5469924434219d4f8adfea5004f
Ryan Brereton-Canham <9389421+ryzr@users.noreply.github.com>
2019-05-30 12:18:41 +0930

    Access single item resource lists at index 0
    https://www.terraform.io/upgrade-guides/0-12.html#working-with-count-on-resources

--- main.tf
+++ main.tf
@@ -26,19 +26,19 @@ resource "aws_route53_record" "validation" {
   count = "${var.create_certificate && var.validation_method == "DNS" && var.validate_certificate ? length(var.subject_alternative_names) + 1 : 0}"
 
   zone_id = "${var.zone_id}"
-  name    = "${lookup(aws_acm_certificate.this.domain_validation_options[count.index], "resource_record_name")}"
-  type    = "${lookup(aws_acm_certificate.this.domain_validation_options[count.index], "resource_record_type")}"
+  name    = "${lookup(aws_acm_certificate.this[0].domain_validation_options[count.index], "resource_record_name")}"
+  type    = "${lookup(aws_acm_certificate.this[0].domain_validation_options[count.index], "resource_record_type")}"
   ttl     = 60
 
   records = [
-    "${lookup(aws_acm_certificate.this.domain_validation_options[count.index], "resource_record_value")}"
+    "${lookup(aws_acm_certificate.this[0].domain_validation_options[count.index], "resource_record_value")}"
   ]
 }
 
 resource "aws_acm_certificate_validation" "this" {
   count = "${var.create_certificate && var.validation_method == "DNS" && var.validate_certificate && var.wait_for_validation ? 1 : 0}"
 
-  certificate_arn = "${aws_acm_certificate.this.arn}"
+  certificate_arn = "${aws_acm_certificate.this[0].arn}"
 
   validation_record_fqdns = [
     "${aws_route53_record.validation.*.fqdn}",
