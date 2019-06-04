920a1e327335590f6546be53783834ed7b8e6f1c
Lowe Schmidt <github@loweschmidt.se>
2019-06-04 10:55:31 +0200

    Refactor to 0.12 syntax.

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_acm_certificate" "this" {
-  count = "${var.create_certificate}"
+  count = var.create_certificate ? 1 : 0
 
-  domain_name               = "${var.domain_name}"
-  subject_alternative_names = ["${var.subject_alternative_names}"]
-  validation_method         = "${var.validation_method}"
+  domain_name               = var.domain_name
+  subject_alternative_names = var.subject_alternative_names
+  validation_method         = var.validation_method
 
-  tags = "${var.tags}"
+  tags = var.tags
 
   lifecycle {
     create_before_destroy = true
@@ -23,21 +23,20 @@ resource "aws_acm_certificate" "this" {
 //}
 
 resource "aws_route53_record" "validation" {
-  count = "${var.create_certificate && var.validation_method == "DNS" && var.validate_certificate ? length(var.subject_alternative_names) + 1 : 0}"
+  count = var.create_certificate && var.validation_method == "DNS" && var.validate_certificate ? length(var.subject_alternative_names) : 0
 
-  zone_id = "${var.zone_id}"
-  name    = "${lookup(aws_acm_certificate.this.domain_validation_options[count.index], "resource_record_name")}"
-  type    = "${lookup(aws_acm_certificate.this.domain_validation_options[count.index], "resource_record_type")}"
+  zone_id = var.zone_id
+  name    = lookup(aws_acm_certificate.this[count.index].domain_validation_options, "resource_record_name")
+  type    = lookup(aws_acm_certificate.this[count.index].domain_validation_options, "resource_record_type")
+  records = lookup(aws_acm_certificate.this[count.index].domain_validation_options, "resource_record_value")
   ttl     = 60
-  records = ["${lookup(aws_acm_certificate.this.domain_validation_options[count.index], "resource_record_value")}"]
 }
 
 resource "aws_acm_certificate_validation" "this" {
-  count = "${var.create_certificate && var.validation_method == "DNS" && var.validate_certificate && var.wait_for_validation ? 1 : 0}"
+  count = var.create_certificate && var.validation_method == "DNS" && var.validate_certificate && var.wait_for_validation ? 1 : 0
 
-  certificate_arn = "${aws_acm_certificate.this.arn}"
+  certificate_arn = aws_acm_certificate.this[count.index].arn
 
-  validation_record_fqdns = [
-    "${aws_route53_record.validation.*.fqdn}",
-  ]
+  validation_record_fqdns = aws_route53_record.validation[*].fqdn
+
 }
