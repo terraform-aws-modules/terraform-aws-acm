c9e1f34820cff268350c74ebac39b9fd3069bf27
Anton Babenko <anton@antonbabenko.com>
2019-06-05 13:31:31 +0200

    Upgraded to Terraform 0.12 and improved support for wildcard domain names

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,29 @@
 output "this_acm_certificate_arn" {
   description = "The ARN of the certificate"
-  value       = "${element(concat(aws_acm_certificate.this.*.arn, list("")), 0)}"
+  value       = element(concat(aws_acm_certificate.this.*.arn, [""]), 0)
 }
 
 output "this_acm_certificate_domain_validation_options" {
   description = "A list of attributes to feed into other resources to complete certificate validation. Can have more than one element, e.g. if SANs are defined. Only set if DNS-validation was used."
-  value       = "${flatten(aws_acm_certificate.this.*.domain_validation_options)}"
+  value       = flatten(aws_acm_certificate.this.*.domain_validation_options)
 }
 
 output "this_acm_certificate_validation_emails" {
   description = "A list of addresses that received a validation E-Mail. Only set if EMAIL-validation was used."
-  value       = "${flatten(aws_acm_certificate.this.*.validation_emails)}"
+  value       = flatten(aws_acm_certificate.this.*.validation_emails)
+}
+
+output "validation_route53_record_fqdns" {
+  description = "List of FQDNs built using the zone domain and name."
+  value       = [] #aws_route53_record.validation.*.fqdn
+}
+
+output "distinct_domain_names" {
+  description = "List of distinct domains names used for the validation."
+  value       = local.distinct_domain_names
+}
+
+output "validation_domains" {
+  description = "List of distinct domain validation options. This is useful if subject alternative names contain wildcards."
+  value       = local.validation_domains
 }
--- variables.tf
+++ variables.tf
@@ -1,40 +1,53 @@
 variable "create_certificate" {
   description = "Whether to create ACM certificate"
+  type        = bool
   default     = true
 }
 
 variable "validate_certificate" {
   description = "Whether to validate certificate by creating Route53 record"
+  type        = bool
+  default     = true
+}
+
+variable "validation_allow_overwrite_records" {
+  description = "Whether to allow overwrite of Route53 records"
+  type        = bool
   default     = true
 }
 
 variable "wait_for_validation" {
   description = "Whether to wait for the validation to complete"
+  type        = bool
   default     = true
 }
 
 variable "domain_name" {
   description = "A domain name for which the certificate should be issued"
+  type        = string
   default     = ""
 }
 
 variable "subject_alternative_names" {
   description = "A list of domains that should be SANs in the issued certificate"
+  type        = list(string)
   default     = []
 }
 
 variable "validation_method" {
   description = "Which method to use for validation. DNS or EMAIL are valid, NONE can be used for certificates that were imported into ACM and then into Terraform."
+  type        = string
   default     = "DNS"
 }
 
 variable "zone_id" {
   description = "The ID of the hosted zone to contain this record."
+  type        = string
   default     = ""
 }
 
 variable "tags" {
   description = "A mapping of tags to assign to the resource"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
