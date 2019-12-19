dcad414fba4aae426f11a3b8fb3b35e8d2453a50
Anton Babenko <anton@antonbabenko.com>
2019-12-19 17:42:24 +0100

    Updated example with multiple SANs after PR #32

--- examples/complete-dns-validation/main.tf
+++ examples/complete-dns-validation/main.tf
@@ -1,26 +1,42 @@
-variable "domain_name" {
-  description = "Domain name to use as Route53 zone and ACM certificate"
-  default     = "my-domain-name.com"
+locals {
+  # Use existing (via data source) or create new zone (will fail validation, if zone is not reachable)
+  use_existing_route53_zone = true
+
+  domain = "terraform-aws-modules.modules.tf"
+
+  # Removing trailing dot from domain - just to be sure :)
+  domain_name = trimsuffix(local.domain, ".")
+}
+
+data "aws_route53_zone" "this" {
+  count = local.use_existing_route53_zone ? 1 : 0
+
+  name         = local.domain_name
+  private_zone = false
 }
 
 resource "aws_route53_zone" "this" {
-  name = var.domain_name
+  count = ! local.use_existing_route53_zone ? 1 : 0
+  name  = local.domain_name
 }
 
 module "acm" {
   source = "../../"
 
-  domain_name = var.domain_name
-  zone_id     = aws_route53_zone.this.zone_id
+  domain_name = local.domain_name
+  zone_id     = coalescelist(data.aws_route53_zone.this.*.zone_id, aws_route53_zone.this.*.zone_id)[0]
 
   subject_alternative_names = [
-    "*.${var.domain_name}",
-    "new.sub.${var.domain_name}",
+    "*.alerts.${local.domain_name}",
+    "*.something.${local.domain_name}",
+    "*.news.${local.domain_name}",
+    "*.info.${local.domain_name}",
+    "new.sub.${local.domain_name}",
   ]
 
-  wait_for_validation = false # true
+  wait_for_validation = true
 
   tags = {
-    Name = var.domain_name
+    Name = local.domain_name
   }
 }
