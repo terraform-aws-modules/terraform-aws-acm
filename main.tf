09a9ac412bcf81d2d2c51599894153ff6d092eeb
David Raphael <dave@singularity.consulting>
2020-06-10 12:55:54 -0500

    Fixed syntax error in newly added options block for certificate trans… (#50)

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ resource "aws_acm_certificate" "this" {
   domain_name               = var.domain_name
   subject_alternative_names = var.subject_alternative_names
   validation_method         = var.validation_method
-  options = {
+
+  options {
     certificate_transparency_logging_preference = var.certificate_transparency_logging_preference ? "ENABLED" : "DISABLED"
   }
-
 
   tags = var.tags
 
