d1c36521822742e90d557ee5183568e6c17b986b
Michel Zanini <michel.zanini@mttnow.com>
2020-08-06 09:39:48 -0300

    fix: creation of route53 validation records when the main domain name starts with star

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   // Get distinct list of domains and SANs
-  distinct_domain_names = distinct(concat([var.domain_name], [for s in var.subject_alternative_names : replace(s, "*.", "")]))
+  distinct_domain_names = distinct(concat([replace(var.domain_name, "*.", "")], [for s in var.subject_alternative_names : replace(s, "*.", "")]))
 
   // Copy domain_validation_options for the distinct domain names
   validation_domains = var.create_certificate ? [for k, v in aws_acm_certificate.this[0].domain_validation_options : tomap(v) if contains(local.distinct_domain_names, replace(v.domain_name, "*.", ""))] : []
