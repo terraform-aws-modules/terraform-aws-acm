460505c8507875ddbf287503973e47cd58c8db6a
Anton Babenko <anton@antonbabenko.com>
2022-06-15 22:09:54 +0200

    Fixed example a bit

--- examples/complete-email-validation-with-validation-domain/main.tf
+++ examples/complete-email-validation-with-validation-domain/main.tf
@@ -8,11 +8,12 @@ module "acm" {
   domain_name = var.domain_name
   zone_id     = aws_route53_zone.this.zone_id
 
-  # the key is the domain name which you want to change the validation domain for. validation emails will be send to a fixed list of recipients:
+  # The key is the domain name which you want to change the validation domain for.
+  # Validation emails will be send to a fixed list of recipients:
   # admin@VALIDATION_DOMAIN, administrator@VALIDATION_DOMAIN, hostmaster@VALIDATION_DOMAIN, postmaster@VALIDATION_DOMAIN, webmaster@VALIDATION_DOMAIN
   # validation_domain has to be a top-level domain of the actual domain
   validation_option = {
-    "${var.domain_name}" = {
+    (var.domain_name) = {
       validation_domain = var.validation_domain
     }
   }
