f225b12d4aadeb49c8f6c58375ad5a35e71f6211
oleksiidv <109069332+oleksiidv@users.noreply.github.com>
2022-08-01 17:40:06 +0300

    Update ssm_outputs_shared.tf

--- ssm_outputs_shared.tf
+++ ssm_outputs_shared.tf
@@ -1,5 +1,4 @@
 resource "aws_ssm_parameter" "acm_certificate_arn" {
   name        = "/shared/acm_certificate_arn"
   description = "ACM ARN"
-  type        = "StringList"
   value       = element(concat(aws_acm_certificate_validation.this.*.certificate_arn, aws_acm_certificate.this.*.arn, [""]), 0)
